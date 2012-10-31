000ea5c36b5e76ca0bc1d7b88cb1aa0eefc6ce90
Akiva Leeder <akiva.leeder@gmail.com>
2012-10-31 17:38:03 -0400

    first commit

---
+++ app/models/programmer.rb
@@ -0,0 +1,10 @@
+class Programmer < ActiveRecord::Base
+  attr_accessible :name, :tag_line, :sidebox_info, :image_url, :wiki_url
+
+
+end
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Programmerstoknow::Application.config.secret_token = '6da61c258344f1d01bba70cd7bfa9e38cce8e4555bb3db0caaa53633e59f8c1cf087864a41b37befb1506ddf840c48f06bf6cc7345dbe2b3c020cdc209eecccb'
